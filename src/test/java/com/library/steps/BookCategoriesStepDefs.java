2e28504bdacaf0cf033b7f18e4617ea0d5bbda9c
cemilkutay <kutaytinmaz@gmail.com>
2023-04-01 13:39:08 +0200

    us03

--- src/test/java/com/library/steps/BookCategoriesStepDefs.java
+++ src/test/java/com/library/steps/BookCategoriesStepDefs.java
@@ -1,4 +1,19 @@
 package com.library.steps;
 
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class BookCategoriesStepDefs {
+
+    @When("the user clicks book categories_CKT")
+    public void the_user_clicks_book_categories_ckt() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("verify book categories must match book_categories table from db_CKT")
+    public void verify_book_categories_must_match_book_categories_table_from_db_ckt() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
