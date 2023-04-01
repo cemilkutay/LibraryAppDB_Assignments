f5b3089893ca57f0a14c0ec018f0cfdd96615905
cemilkutay <kutaytinmaz@gmail.com>
2023-04-01 13:35:27 +0200

    us03

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/library/steps",
         dryRun = true,
-        tags = "wip",
+        tags = "@wip",
         publish = true
 )
 public class CukesRunner {
